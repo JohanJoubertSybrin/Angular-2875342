bc596691e759c9a1a0873c5150ecd748dd9a15ae
Johan <121931753+JohanJoubertSybrin@users.noreply.github.com>
2023-01-31 14:47:11 +0200

    03-03

--- src/app/services/web-storage.service.ts
+++ src/app/services/web-storage.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class WebStorageService {
 
   constructor() { }
+
+  public get(key: string): string | null {
+    return window.localStorage.getItem(key);
+  }
+
+  public set(key: string, value: string): void {
+    window.localStorage.setItem(key, value);
+  }
 }

5c79bb8dee0ed4d6cc23b0f5e53c35fe1ab23e95
Johan <121931753+JohanJoubertSybrin@users.noreply.github.com>
2023-01-31 12:54:00 +0200

    .

---
+++ src/app/service/error-metadata.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, ErrorHandler } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorMetadataService implements ErrorHandler {
+  public handleError(error:any): void {
+    const date = new Date();
+
+    console.error({
+      timestamp: date.toISOString(),
+      message: error.message,
+      zone: error.zone
+    });
+  }
+}
